53f83bb08cbe3396ae3a5e388fe3d3388462a8c3
Stephane <son.trinh.dev@gmail.com>
2017-12-29 04:52:08 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -11,3 +11,14 @@ for (i = 0; i < gifts.length; i++) {
 for (var i = 0; i < 100; i +=5) {
   console.log("Hello world the " + i + " time")
 }
+
+function forLoop(array) {
+  for (i = 0; i < 25; i++) {
+    if (i ===1) {
+      array.push(`I am a 1 strange loop.`)
+    } else {
+      
+    }
+    array.push(`I am #{i} string loops.`)
+  }
+}
