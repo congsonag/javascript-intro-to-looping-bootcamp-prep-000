c79e396a846bad91f4d2ffa218e67fbdf5ef1b6f
Stephane <son.trinh.dev@gmail.com>
2017-12-29 04:52:40 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,24 +1,23 @@
-var gifts = ['teddy bear', 'drone', 'doll']
-
-function wrapGift(gift) {
-  console.log(`Wrapped ${gift} and andded a bow!`)
-}
-
-for (i = 0; i < gifts.length; i++) {
-  wrapGift(gifts[i])
-}
-
-for (var i = 0; i < 100; i +=5) {
-  console.log("Hello world the " + i + " time")
-}
+// var gifts = ['teddy bear', 'drone', 'doll']
+// 
+// function wrapGift(gift) {
+//   console.log(`Wrapped ${gift} and andded a bow!`)
+// }
+// 
+// for (i = 0; i < gifts.length; i++) {
+//   wrapGift(gifts[i])
+// }
+// 
+// for (var i = 0; i < 100; i +=5) {
+//   console.log("Hello world the " + i + " time")
+// }
 
 function forLoop(array) {
   for (i = 0; i < 25; i++) {
     if (i ===1) {
       array.push(`I am a 1 strange loop.`)
     } else {
-      
-    }
-    array.push(`I am #{i} string loops.`)
+      array.push(`I am #{i} string loops.`)
+    }  
   }
 }
