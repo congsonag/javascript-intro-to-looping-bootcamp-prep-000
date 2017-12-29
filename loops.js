a399c7679669b4df7675e80335971c7b4ecb88ff
Stephane <son.trinh.dev@gmail.com>
2017-12-29 04:56:43 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -23,8 +23,8 @@ function forLoop(array) {
   return array
 }
 
-let countdown = 100;
 
-while (countdown > 0) {
-  console.log(countdown--)
+function whileLoop(n) {
+  console.log(n --)
+  return 'done'
 }
