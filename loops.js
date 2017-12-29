a08f1d596a19e6611c9ae3d2da55341fff5c4fe3
Stephane <son.trinh.dev@gmail.com>
2017-12-29 04:52:49 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,13 +1,13 @@
 // var gifts = ['teddy bear', 'drone', 'doll']
-// 
+//
 // function wrapGift(gift) {
 //   console.log(`Wrapped ${gift} and andded a bow!`)
 // }
-// 
+//
 // for (i = 0; i < gifts.length; i++) {
 //   wrapGift(gifts[i])
 // }
-// 
+//
 // for (var i = 0; i < 100; i +=5) {
 //   console.log("Hello world the " + i + " time")
 // }
@@ -18,6 +18,6 @@ function forLoop(array) {
       array.push(`I am a 1 strange loop.`)
     } else {
       array.push(`I am #{i} string loops.`)
-    }  
+    }
   }
 }
